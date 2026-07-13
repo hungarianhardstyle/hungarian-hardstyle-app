2c4ce36ee48279c988ecdcf3ee6eb8ce39aba10c
hungarianhardstyle <djdeeroy@gmail.com>
2026-07-13 18:05:26 +0200

    Refresh plugin registration and restore test ads

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'core/theme/app_theme.dart';
 import 'screens/main_navigation.dart';
@@ -10,6 +11,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting('hu_HU');
+  await MobileAds.instance.initialize();
   runApp(const ProviderScope(child: HungarianHardstyleApp()));
 }
 
--- lib/providers/ads_provider.dart
+++ lib/providers/ads_provider.dart
@@ -1,4 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// Disabled until the native AdMob setup is verified on the target device.
-final adsEnabledProvider = Provider<bool>((ref) => false);
+final adsEnabledProvider = Provider<bool>((ref) => true);
